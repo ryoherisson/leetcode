a4c882145373c3a0850ebe9bd93f2e02a75b477f
ryoherisson <ryhizw@gmail.com>
2022-10-02 22:29:32 +0900

    0001

--- python/leetcode0001-two_sum.py
+++ python/leetcode0001-two_sum.py
@@ -4,32 +4,44 @@ Solution ideas
     time O(n^2), space: O(1)
 2. Sort
     time O(nlogn), space: O(1)
+3. Hashmap
+    time O(n), space: O(n)
 '''
 
 
 from typing import List
 
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         nums_sorted = sorted(nums)
+#         left = 0
+#         right = len(nums_sorted) - 1
+
+#         while left < right:
+#             sum = nums_sorted[left] + nums_sorted[right]
+#             if sum == target:
+#                 break
+
+#             if sum > target:
+#                 right -= 1
+#             else:
+#                 left += 1
+
+#         res = []
+#         for i, num in enumerate(nums):
+#             if num == nums_sorted[left]:
+#                 res.append(i)
+#             elif num == nums_sorted[right]:
+#                 res.append(i)
+#         return res
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_sorted = sorted(nums)
-        left = 0
-        right = len(nums_sorted) - 1
+        prev_map = {}
 
-        while left < right:
-            sum = nums_sorted[left] + nums_sorted[right]
-            if sum == target:
-                break
-
-            if sum > target:
-                right -= 1
-            else:
-                left += 1
-
-        res = []
-        for i, num in enumerate(nums):
-            if num == nums_sorted[left]:
-                res.append(i)
-            elif num == nums_sorted[right]:
-                res.append(i)
-        return res
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prev_map:
+                return [prev_map[diff], i]
+            prev_map[n] = i
+        return
