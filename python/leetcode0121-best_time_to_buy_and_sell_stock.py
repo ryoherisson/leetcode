3065b78564dd75dc0a5922208d56bfad75ca34da
ryoherisson <ryhizw@gmail.com>
2022-10-03 09:10:21 +0900

    121

---
+++ python/leetcode0121-best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+from typing import Type
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         res = 0
+
+#         current_price = prices[0]
+
+#         for i, p in enumerate(prices):
+#             if i > 0:
+#                 if p - current_price > res:
+#                     res = p - current_price
+#                 elif p < current_price:
+#                     current_price = p
+
+#         return res
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        max_p = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_p = max(max_p, profit)
+            else:
+                l = r
+            r += 1
+
+        return max_p
