8c170ebb0791bb6c30c6e4a34f268d1ce78c5358
ryoherisson <ryhizw@gmail.com>
2022-10-04 20:15:19 +0900

    121

--- python/leetcode0121-best_time_to_buy_and_sell_stock.py
+++ python/leetcode0121-best_time_to_buy_and_sell_stock.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-from typing import Type
+from typing import List
 
 # class Solution:
 #     def maxProfit(self, prices: List[int]) -> int:
