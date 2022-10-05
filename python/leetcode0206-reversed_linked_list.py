8db1b1c6b27f8b487f2819d06f1f68a1a6ecc39c
ryoherisson <ryhizw@gmail.com>
2022-10-05 23:49:52 +0900

    206

---
+++ python/leetcode0206-reversed_linked_list.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp.next
+
+        return curr
