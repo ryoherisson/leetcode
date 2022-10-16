08cc344367cd2e36dfa9986dc96091efa6bffea5
ryoherisson <ryhizw@gmail.com>
2022-10-17 06:53:49 +0900

    191

---
+++ python/leetcode0191-number_of_1bits.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/number-of-1-bits/
+
+class Solution1:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+
+        while n:
+            res += n % 2
+            n = n >> 1
+
+        return res
+
+
+class Solution2:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            n &= (n - 1)
+            res += 1
+        return res
