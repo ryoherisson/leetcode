6d8cfbceccec42ff88b55df64ba0b7a1d609c060
ryoherisson <ryhizw@gmail.com>
2022-10-21 09:18:16 +0900

    202

--- python/leetcode0202-happy_number.py
+++ python/leetcode0202-happy_number.py
@@ -1,9 +1,9 @@
 # https://leetcode.com/problems/happy-number/
 
 
-class Solution:
+class Solution1:
     def isHappy(self, n: int) -> bool:
-        visit = set() # Memory O(n)
+        visit = set()  # Memory O(n)
 
         while n not in visit:
             visit.add(n)
@@ -13,6 +13,29 @@ class Solution:
                 return True
 
         return False
+
+    def sum_of_square(self, n: int) -> int:
+        output = 0
+
+        while n:
+            output += (n % 10)**2
+            n //= 10
+
+        return output
+
+
+class Solution2:
+    def isHappy(self, n: int) -> bool:
+        slow, fast = n, n
+
+        while fast != 1:
+            slow = self.sum_of_square(slow)
+            fast = self.sum_of_square(self.sum_of_square(fast))
+
+            if slow != 1 and slow == fast:
+                return False
+
+        return True
 
     def sum_of_square(self, n: int) -> int:
         output = 0
