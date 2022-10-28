bfbc5ff72829f94a742e431908f833c6528e0844
ryoherisson <ryhizw@gmail.com>
2022-10-28 21:56:16 +0900

    238

--- python/leetcode0238-product_of_array_except_self.py
+++ python/leetcode0238-product_of_array_except_self.py
@@ -3,15 +3,45 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1]
-        for i in range(1, len(nums)):
-            res[0] *= nums[i]
+        res = [1] * len(nums)
+        zero_count = 0
+        product = 1
 
+        for i in range(0, len(nums)):
+            if nums[i] == 0:
+                zero_count += 1
+                continue
 
-        for i in range(1, len(nums)):
-            res.append(res[i-1] // nums[i] * nums[i-1])
+            product *= nums[i]
 
+        if zero_count > 1:
+            return [0] * len(nums)
+
+        for i in range(0, len(nums)):
+            if zero_count == 1:
+                if nums[i] == 0:
+                    res[i] = product
+                else:
+                    res[i] = 0
+            else:
+                res[i] = product // nums[i]
+
+        return res
+
+class Solution2:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
 
         return res
